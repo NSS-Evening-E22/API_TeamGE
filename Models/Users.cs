3d980ccc950b9aefb474f542cce87c2f56835ef8
ahamm <ahammer753@gmail.com>
2023-08-07 18:42:17 -0500

    Users class and data and sample data set up

---
+++ Models/Users.cs
@@ -0,0 +1,15 @@
+﻿namespace API_TeamGE.Models
+{
+    public class Users
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Bio { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ProfileImage { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
